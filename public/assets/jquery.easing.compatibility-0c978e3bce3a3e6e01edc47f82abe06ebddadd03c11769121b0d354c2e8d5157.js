8e61ea0bb718a17526a2c07ebc27b9cd09ea205e
沈旭芷 <caroline.li9@yahoo.com>
2024-10-03 21:43:57 -0500

    fix Function has 5 arguments issues

--- public/assets/jquery.easing.compatibility-0c978e3bce3a3e6e01edc47f82abe06ebddadd03c11769121b0d354c2e8d5157.js
+++ public/assets/jquery.easing.compatibility-0c978e3bce3a3e6e01edc47f82abe06ebddadd03c11769121b0d354c2e8d5157.js
@@ -15,46 +15,60 @@ $.extend( $.easing,
 		const {x, t, b, c, d} = options;
 		return $.easing.easeInQuad(x, t, b, c, d);
 	},
-	easeOut: function (x, t, b, c, d) {
+	easeOut: function (options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeOutQuad(x, t, b, c, d);
 	},
-	easeInOut: function (x, t, b, c, d) {
+	easeInOut: function (options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInOutQuad(x, t, b, c, d);
 	},
-	expoin: function(x, t, b, c, d) {
+	expoin: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInExpo(x, t, b, c, d);
 	},
-	expoout: function(x, t, b, c, d) {
+	expoout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeOutExpo(x, t, b, c, d);
 	},
-	expoinout: function(x, t, b, c, d) {
+	expoinout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInOutExpo(x, t, b, c, d);
 	},
-	bouncein: function(x, t, b, c, d) {
+	bouncein: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInBounce(x, t, b, c, d);
 	},
-	bounceout: function(x, t, b, c, d) {
+	bounceout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeOutBounce(x, t, b, c, d);
 	},
-	bounceinout: function(x, t, b, c, d) {
+	bounceinout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInOutBounce(x, t, b, c, d);
 	},
-	elasin: function(x, t, b, c, d) {
+	elasin: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInElastic(x, t, b, c, d);
 	},
-	elasout: function(x, t, b, c, d) {
+	elasout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeOutElastic(x, t, b, c, d);
 	},
-	elasinout: function(x, t, b, c, d) {
+	elasinout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInOutElastic(x, t, b, c, d);
 	},
-	backin: function(x, t, b, c, d) {
+	backin: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInBack(x, t, b, c, d);
 	},
-	backout: function(x, t, b, c, d) {
+	backout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeOutBack(x, t, b, c, d);
 	},
-	backinout: function(x, t, b, c, d) {
+	backinout: function(options) {
+		const {x, t, b, c, d} = options;
 		return $.easing.easeInOutBack(x, t, b, c, d);
 	}
 });})(jQuery);
